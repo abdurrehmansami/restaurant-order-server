9da4188fb8affcced5bbd41203610ff37eced91c
abdurrehmansami <abdurrehmansherazi190@gmail.com>
2024-07-29 20:19:51 +0500

    added category

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,16 @@
 const Product = require('../models/product');
+const Category = require('../models/category');
 
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.findAll();
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['name'] // Include only the category name, you can include other attributes if needed
+        }
+      ]
+    });
     res.json(products);
   } catch (error) {
     res.status(500).send(error.message);
